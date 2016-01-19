f66efb51cfc86b782a7d4726ea6b5a9f8464fe7a
xiongwilee <birthday10241120@qq.com>
2016-01-19 13:48:08 +0800

    update:更新屏蔽新首页和搜索结果页

--- static/main.js
+++ static/main.js
@@ -1,27 +1,67 @@
+/**
+ * baidu2google:通过chrome浏览器插件修改百度首页为谷歌主题样式：用谷歌百度一下！
+ * @author xiongwilee
+ */
 var setTheme = (function () {
-	var _init = function(){
+	var SetTheme = function(){
+		this.linkDOM;
+	}
+
+	SetTheme.prototype.init = function(){
 		// 载入CSS
-		_initCSS();
+		this.initCSS();
 
-		// 设置头部
-		_setTitle();
+		// 设置头部：标题等
+		this.setTitle();
+
+		// 事件监听
+		this.bindEvent();
 	}
-	var _initCSS = function(){
-		var cssURL = chrome.extension.getURL('static/main.css'),
-			linkDOM = document.createElement('link');
+	/**
+	 * 载入CSS
+	 */
+	SetTheme.prototype.initCSS = function(){
+		// 如果已经登录就不加载CSS
+		if(document.getElementById('s_username_top')){
+			return;
+		}
 
-		linkDOM.rel = 'stylesheet';
-		linkDOM.href = cssURL;
+		var cssURL = chrome.extension.getURL('static/main.css');
+			
+		this.linkDOM = document.createElement('link');
+		this.linkDOM.rel = 'stylesheet';
+		this.linkDOM.href = cssURL;
 
-		document.body.appendChild(linkDOM);
+		document.body.appendChild(this.linkDOM);
 	}
-	var _setTitle = function(){
+	/**
+	 * 删除CSS
+	 */
+	SetTheme.prototype.removeCSS = function(){
+		this.linkDOM && this.linkDOM.remove();
+	}
+	/**
+	 * 设置头部：标题等
+	 */
+	SetTheme.prototype.setTitle = function(){
 		document.title = 'Google';
 	}
+	/**
+	 * 监听事件
+	 */
+	SetTheme.prototype.bindEvent = function(){
+		var me = this,
+			$wrapper = document.getElementById('wrapper');
 
-	return {
-		init : _init
+		// 当输入内容，切搜索视图变化的时候就删掉CSS
+		document.getElementById('kw').addEventListener('keyup', function(){
+			if($wrapper.classList.contains('wrapper_l')){
+				me.removeCSS();
+			}
+		})
 	}
+
+	return (new SetTheme());
 })();
 
 setTheme.init();
