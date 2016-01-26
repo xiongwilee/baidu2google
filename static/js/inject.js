ad2cad0460389c522ea242241a5bfed7e85c63cf
xiongwilee <birthday10241120@qq.com>
2016-01-27 02:26:45 +0800

    update: 通过chrome-extensions-boilerplate编译生成

--- static/js/inject.js
+++ static/js/inject.js
@@ -6,19 +6,15 @@ var setTheme = (function () {
 	var Base = {
 		// 当前是否为已经登录的页面
 		isLogindPage : function(){
-			return !!(document.getElementById('s_username_top') || document.getElementById('user'))
+			return !!(document.getElementById('s_username_top') || document.getElementById('user'));
 		},
 		// 当前是否为搜索结果页
 		isResultPage : function(){
 			return !!document.getElementById('wrapper').classList.contains('wrapper_l');
 		}
-	}
+	};
 
-	var SetTheme = function(){
-		this.linkDOM;
-		this.stat;
-		this.timer;
-	}
+	var SetTheme = function(){};
 
 	SetTheme.prototype.init = function(){
 		// 获取默认状态并存到this.stat变量中
@@ -33,7 +29,7 @@ var setTheme = (function () {
 		this.bindEvent();
 		// 在百度搜索框里输入完之后，回车会删除所有DOM，设置轮询检测是否CSS DOM是否已经删除
 		// this.setTimer();
-	}
+	};
 	/**
 	 * 获取当前状态
 	 */
@@ -44,26 +40,25 @@ var setTheme = (function () {
 		curStatus.isResult = Base.isResultPage();
 
 		if(!curStatus.isLogind && !curStatus.isResult){
-			curStatus.css = chrome.extension.getURL('static/css/main_index_no_login.css')
+			curStatus.css = chrome.extension.getURL('static/css/main_index_no_login.css');
 		}else if(curStatus.isResult){
-			curStatus.css = chrome.extension.getURL('static/css/main_result.css')
+			curStatus.css = chrome.extension.getURL('static/css/main_result.css');
 		}else{
 			curStatus.css = '';
 		}
 
-		return curStatus
-	}
+		return curStatus;
+	};
 	/**
 	 * 当状态发生变化时
 	 */
 	SetTheme.prototype.onChangeStatus = function(){
 		var curStatus = this.getStatus();
-		if(	curStatus.isLogind != this.stat.isLogind 
-			|| curStatus.isResult != this.stat.isResult){
+		if(	curStatus.isLogind != this.stat.isLogind || curStatus.isResult != this.stat.isResult){
 			this.linkDOM.href = curStatus.css;
 			this.stat = curStatus;
 		}
-	}
+	};
 	/**
 	 * 载入CSS
 	 */
@@ -74,7 +69,7 @@ var setTheme = (function () {
 
 		// 把link文件放在</body>和</html>之间，以避免回车搜索时干掉DOM
 		document.body.parentElement.appendChild(this.linkDOM);
-	}
+	};
 	/**
 	 * 设置头部：标题等
 	 */
@@ -93,13 +88,13 @@ var setTheme = (function () {
 		}
 
 		document.title = title;
-	}
+	};
 	/**
 	 设置favicon
 	 */
 	SetTheme.prototype.setFavicon = function () {
 		var oldIcon = document.querySelector('link[type="image/x-icon"]');
-		var newIcon = chrome.extension.getURL('static/image/new_baidu_favicon.ico');
+		var newIcon = chrome.extension.getURL('icons/favicon.ico');
 
 		if( !oldIcon ){
 			oldIcon = document.createElement('link');
@@ -109,9 +104,9 @@ var setTheme = (function () {
 		}
 
 		if( this.stat.isResult || !this.stat.isLogind ){ 
-			oldIcon,oldIcon.href = newIcon;
+			oldIcon.href = newIcon;
 		}
-	}
+	};
 	/**
 	 * 监听事件
 	 */
@@ -121,15 +116,15 @@ var setTheme = (function () {
 		// 当输入内容，切搜索视图变化的时候就删掉CSS
 		document.getElementById('kw').addEventListener('keyup', function(){
 			me.onChangeStatus();
-		})
+		});
 
 		document.getElementById('form').addEventListener('submit', function(){
 			setTimeout(function(){
 				me.setTitle();
 			},500);
 			me.setFavicon();
-		})
-	}
+		});
+	};
 	/**
 	 * 在百度搜索框里输入完之后，回车会删除所有DOM，设置轮询检测是否CSS DOM是否已经删除
 	 */
@@ -138,13 +133,13 @@ var setTheme = (function () {
 
 		clearInterval(this.timer);
 		me.timer = setInterval(function(){
-			if (!!me.linkDOM.parentNode) {return};
+			if (!!me.linkDOM.parentNode) {return;}
 
 			me.initCSS();
 			me.setTitle();
 			me.setFavicon();
 		},500);
-	}
+	};
 
 	return (new SetTheme());
 })();
